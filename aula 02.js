795540157dcb77ce77d17f361ce65d707e391351
Mpompeu41 <89050561+Mpompeu41@users.noreply.github.com>
2021-08-18 20:00:58 -0300

    Add files via upload

---
+++ aula 02.js
@@ -0,0 +1,50 @@
+//console.log('ola mundo!');
+
+//prompt('Digite seu nome:');
+
+//let usuario = 'Pompeu';
+
+//console.log(usuario);
+
+
+//let aluno = prompt('Digite seu nome:');
+//let turma = prompt('Digite sua turma');
+//console.log('aluno' + aluno +', seja bem vindo (a) à turma'+ turma);
+
+
+//let fruta = prompt('digite o nome da fruta:');
+
+//console.log(fruta);
+
+//let minhaVariavel = prompt('Digite seu nome: ')
+//console.log('Boa noite, '+minhaVariavel+', seja bem vindo (a)!');
+
+//let varTexto = 'isso é um string';
+//let varNunmero = 30;
+
+//console.log(varTexto);
+//console.log(varNunmero);
+
+//let arrayNome = ["João","Duda","Matheus","Simone"]
+//let arrayNumero = [10,20,30,40,50];
+//let arrayMisto = ["Jezimiel", false,500];
+
+//cosole.log(batata [3]);
+
+//typeof arrayNome
+
+//var a = "Turma C7";
+//var b = a;
+//console.log(b);
+
+//var a = "Eduardo"
+//console.log(b);
+
+//var c, d = c = "Novissima string"
+//console.log(c);
+//console.log(d);
+
+let = soma
+
+
+
